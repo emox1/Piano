2d2c3d4da03373dd19850fbb9001cdeaa2ecbdc2
emox1 <emox1@naver.com>
2026-01-05 23:04:11 +0900

    first commit

---
+++ src/test/java/com/example/Piano/PianoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Piano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PianoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
